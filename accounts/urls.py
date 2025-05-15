30b2710f056a1c1970f071a70e67504a6a07c3c9
nexdavid2020 <nex_david@naver.com>
2025-05-15 23:20:10 +0900

    url 수정

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from accounts.views import UserRegister_single_APIView
+from accounts.views import *
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
-    path('register/', UserRegister_single_APIView.as_view(), name='register'),
+    path('register-single/', UserRegister_single_APIView.as_view(), name='register'),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
