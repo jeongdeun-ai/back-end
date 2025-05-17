9b415786eff0b5174818f8c2d0a4fa35757e5a1e
nexdavid2020 <nex_david@naver.com>
2025-05-17 17:47:32 +0900

    chat app 생성

---
+++ chat/views.py
@@ -0,0 +1,13 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+from table.models import *
+from serializers import *
+
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import api_view, permission_classes
+from django.db import IntegrityError
+from django.db import transaction
+
+
