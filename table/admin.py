29b626900c1f0239f6850c401ed1c7eadbfffa8c
nexdavid2020 <nex_david@naver.com>
2025-05-17 17:48:33 +0900

    table/admin.py에 릴레이션 추가

--- table/admin.py
+++ table/admin.py
@@ -2,9 +2,13 @@ from django.contrib import admin
 from .models import *
 
 
+
 admin.site.register(User)
 admin.site.register(Parent)
 admin.site.register(UserParentRelation)
 admin.site.register(ChatLog)
 admin.site.register(DailyReport)
 admin.site.register(ContextSummary)
+admin.site.register(Event)
+admin.site.register(MedicationSchedule)
+admin.site.register(MedicationItem)
